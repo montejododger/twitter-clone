1331a270c645b240f8435d6d8a02da4a601a9002
Matthew Montejo <montejododger@yahoo.com>
2024-04-09 16:36:58 -0700

    csrf added

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,44 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const cors = require("cors");
+const csurf = require("csurf");
+const { isProduction } = require("./config/keys");
 
 // IMPOIRT ROUTES FROM API
-const usersRouter = require('./routes/api/users');
-const tweetsRouter = require('./routes/api/tweets');
+const usersRouter = require("./routes/api/users");
+const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// ADD THIS SECURITY MIDDLEWARE
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+);
+
 //ATTACH EXPRESS ROUTERS
-app.use('/api/users', usersRouter);
-app.use('/api/tweets', tweetsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/tweets", tweetsRouter);
+app.use("/api/csrf", csrfRouter);
 
 module.exports = app;
