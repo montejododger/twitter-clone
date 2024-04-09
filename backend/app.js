747afb9f6ee88f9fab3e81b3d6d21a7b0c94de00
Matthew Montejo <montejododger@yahoo.com>
2024-04-09 16:07:45 -0700

    more routes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,21 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// IMPOIRT ROUTES FROM API
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//ATTACH EXPRESS ROUTERS
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
 
 module.exports = app;
